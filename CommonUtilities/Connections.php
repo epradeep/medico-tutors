98a2630aa64f8ffc63e01a146c548245c1a53cf6
Pradeep Etika <etikapradeep@gmail.com>
2020-09-25 11:15:22 +0530

    Updated DB Connection

--- CommonUtilities/Connections.php
+++ CommonUtilities/Connections.php
@@ -2,10 +2,17 @@
 	date_default_timezone_set('Asia/Calcutta');
 
 		
-	$host ="localhost";
+	/*$host ="localhost";
 	$db_name = "medicovi_app";
 	$db_username = "root";
-	$db_password = "";
+	$db_password = "";*/
+
+	$host ="remotemysql.com";
+	$db_name = "QTbGOdLRBF";
+	$db_username = "QTbGOdLRBF";
+	$db_password = "GQNZOHLOfV";
+
+
 	
 	$dbConnection = new PDO("mysql:dbname=$db_name;host=$host;charset=utf8", "$db_username", "$db_password");	
 	$dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);	
